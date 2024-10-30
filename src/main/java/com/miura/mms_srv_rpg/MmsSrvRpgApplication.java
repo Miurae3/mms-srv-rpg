dbd165e514259a65510b68e5a410ece2b7123f2c
Miura <miura.hublab@gmail.com>
2024-10-30 10:01:08 -0300

    initial commit

---
+++ src/main/java/com/miura/mms_srv_rpg/MmsSrvRpgApplication.java
@@ -0,0 +1,13 @@
+package com.miura.mms_srv_rpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmsSrvRpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmsSrvRpgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miura/mms_srv_rpg/MmsSrvRpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miura.mms_srv_rpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmsSrvRpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
